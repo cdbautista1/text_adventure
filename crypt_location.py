84c7c179c8fcd0eb4efe70d515fc28cb8027ce3a
cdbautista1 <132464387+cdbautista1@users.noreply.github.com>
2025-01-03 14:33:30 -0500

    Add files via upload

---
+++ crypt_location.py
@@ -0,0 +1,8 @@
+import random
+from items import warrior_items, mage_items, archer_items
+
+# function that goes through what the user decides to do in the crypt
+def crypt(area):
+    if area == 'Crypt':
+        print('You chose the crypt. ')
+        # you find a book
---
+++ weather.py
@@ -0,0 +1,1 @@
+weather = ['rainy', 'sunny', 'foggy']
